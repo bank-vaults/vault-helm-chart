268e7c59d8b04460771fb10cb2573a4425cfb9db
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-06-20 12:55:37 +0200

    chore: migration
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ README.md
@@ -0,0 +1,36 @@
+# Vault Helm chart
+
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/bank-vaults/vault-helm-chart/ci.yaml?style=flat-square)](https://github.com/bank-vaults/vault-helm-chart/actions/workflows/ci.yaml)
+[![OpenSSF Scorecard](https://api.securityscorecards.dev/projects/github.com/bank-vaults/vault-helm-chart/badge?style=flat-square)](https://api.securityscorecards.dev/projects/github.com/bank-vaults/vault-helm-chart)
+
+**A Helm chart for installing Hashicorp Vault .**
+
+## Development
+
+**For an optimal developer experience, it is recommended to install [Nix](https://nixos.org/download.html) and [direnv](https://direnv.net/docs/installation.html).**
+
+_Alternatively, install [Go](https://go.dev/dl/) on your computer then run `make deps` to install the rest of the dependencies._
+
+Make sure Docker is installed with Compose and Buildx.
+
+Run project dependencies:
+
+```shell
+make up
+```
+
+Run the test suite:
+
+```shell
+make test-acceptance
+```
+
+Run linters:
+
+```shell
+make lint # pass -j option to run them in parallel
+```
+
+## License
+
+The project is licensed under the [Apache 2.0 License](LICENSE).
---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "A Helm chart for installing Hashicorp Vault";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+        devenv.shells = {
+          default = {
+            languages = {
+              go.enable = true;
+            };
+
+            pre-commit.hooks = {
+              nixpkgs-fmt.enable = true;
+              yamllint.enable = true;
+            };
+
+            packages = with pkgs; [
+              gnumake
+
+              kind
+              kubectl
+              kubectl-images
+              kustomize
+              kubernetes-helm
+              helm-docs
+
+              yamllint
+            ];
+
+            scripts = {
+              versions.exec = ''
+                go version
+                kind version
+                kubectl version --client
+                echo kustomize $(kustomize version --short)
+                echo helm $(helm version --short)
+              '';
+            };
+
+            enterShell = ''
+              versions
+            '';
+
+            # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
+            containers = pkgs.lib.mkForce { };
+          };
+
+          ci = devenv.shells.default;
+        };
+      };
+    };
+}
